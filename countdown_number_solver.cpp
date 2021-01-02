6c21e629dce7990e74137aea341ff27aa8c270db
Owen Elliott <u5788450@anu.edu.au>
2021-01-02 16:53:02 +1100

    intial

--- countdown_number_solver.cpp
+++ countdown_number_solver.cpp
@@ -17,23 +17,14 @@ void print_vector(vector<int> &input) {
     cout << "\n";
 }
 
-// returns all possible pairwise combinations of numbers in a vector (not permutations)
-vector<tuple<int, int>> combinations(vector<int> input) {
-    vector<tuple<int, int>> output;
-    for (int i = 0; i < input.size(); i++) {
-        for (int j = i+1; j < input.size(); j++) {
-            output.push_back(make_tuple(input[i], input[j]));
-        }
-    }
-    return output;
-}
-
 // helper to return an 'action' given an operation code
-int perform_operation(int num1, int num2, int opCode) {
+int perform_operation(const int &num1, const int &num2, const int &opCode) {
     if (opCode==0) {
         return num1 + num2;
     }
     else if (opCode==1) {
+        if (num1==num2)
+            return -1;
         return num1 - num2;
     }
     else if (opCode==2) {
@@ -51,82 +42,88 @@ int perform_operation(int num1, int num2, int opCode) {
 
 // helper function to efficiently remove one occurance of an element from a vector - avoids the need for elements
 // above the one being replaced to be copied down
-vector<int> replace_once(vector<int> vec, int el) {
+void replace_once(vector<int> & vec, int el) {
     auto oc = find(vec.begin(), vec.end(), el);
     if (oc != vec.end()) {
         iter_swap(oc, vec.end() - 1);
         vec.erase(vec.end() - 1);
     }
-    return vec;
 }
 
 // main solving method
-vector<string> solve(vector<int> numbers, int goal) {
+void solve(vector<int> numbers, const int goal, const int nsols) {
 
     // initialise the deques for the numbers and the path (used in tandem)
     // deque will be used as FIFO
     deque<vector<int>> numQ; 
-    deque<vector<string>> pthQ;
+    deque<string> pthQ;
     numQ.push_front(numbers);
-    vector<string> path = {""};
+    string path = {""};
     pthQ.push_front(path);
 
     // to make printing the path taken easier
-    unordered_map<int, string> opCodetoOp = {{0, "add"}, {1, "subtract"}, {2, "multiply"}, {3,"divide"}};
+    unordered_map<int, string> opCodetoOp = {{0, "Add"}, {1, "Subtract"}, {2, "Multiply"}, {3, "Divide"}};
 
     // count the nodes expanded
     int nodes = 0;
-
+    int sols = 1;
     // while the queue has elements
     while (!numQ.empty()) {
-        vector<int> nums = numQ.back();
-        vector<string> path = pthQ.back();
-        numQ.pop_back();
-        pthQ.pop_back();
+        vector<int> nums = numQ.front();
+        string path = pthQ.front();
+        numQ.pop_front();
+        pthQ.pop_front();
 
         // if the target number is in the numbers then return the path as we have achieved a solution
-        if (count(nums.begin(), nums.end(), goal)) {
-            cout << "Solution found after expanding " << nodes << " nodes!\n";
-            return path;
+        if (nums.back() == goal) {
+            cout << "Solution " << sols << " found after expanding " << nodes << " nodes!\n";
+            cout << path;
+            ++sols;
+            if (sols>nsols)
+                return;
+            cout << "\n";
         } else {
-            vector<tuple<int, int>> combs = combinations(nums);
-            for(auto const& tup: combs) {
-                int num1 = get<0>(tup);
-                int num2 = get<1>(tup);
+            for (int i = 0; i < nums.size()-1; i++) {
+                // the trick is that you never need more that 1 intermediate result
+                // as such our second number only has to be one of the last 2 calculated but not yet used
+                int start;
+                if (i<nums.size()-2)
+                    start = nums.size()-2;
+                else 
+                    start = nums.size()-1;
+                for (int j = start; j < nums.size(); j++) {
+                    int num1 = max(nums[i], nums[j]);
+                    int num2 = min(nums[i], nums[j]);
 
-                for (int i=0; i<4; i++) {
-                    int result = perform_operation(num1, num2, i);
-                    if (result < 0) 
-                        continue;
-                    ++nodes; // increment the nodes expanded
-                    vector<int> new_nums = replace_once(nums, num1);
-                    new_nums = replace_once(new_nums, num2);
+                    for (int x=0; x<4; x++) {
+                        int result = perform_operation(num1, num2, x);
+                        if (result <= 0 || result == num1 || result == num2) 
+                            continue;
 
-                    string step = opCodetoOp[i] + " " + to_string(max(num1, num2)) + " and " + to_string(min(num1, num2)) + " to get " + to_string(result);
-                    
-                    vector<string> new_path = path;
-                    new_path.push_back(step);
-                    new_nums.push_back(result);
-
-                    numQ.push_front(new_nums);
-                    pthQ.push_front(new_path);
+                        vector<int> new_nums = nums;
+                        replace_once(new_nums, num1);
+                        replace_once(new_nums, num2);
+                        new_nums.push_back(result);
+                        
+                        string new_path = path + opCodetoOp[x] + " " + to_string(num1) + " and " + to_string(num2) + " to get " + to_string(result) + "\n";
+                        
+                        // increment the nodes expanded
+                        ++nodes;
+                        numQ.push_front(new_nums);
+                        pthQ.push_front(new_path);
+                    }
                 }
-
             }
         }
     }
-    cout << "No solution exists, " << nodes << " nodes were expanded";
-    vector<string> fail = {"No Solution :("};
-    return fail;
+    cout << "No more solutions exist :( - " << nodes << " nodes were expanded\n";
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    // prompt for numbers
-    cout << "\nEnter the numbers: \n";
-    string numberStr;
-    cin >> numberStr;
+    string numberStr = argv[1];
+
 
     // get these numbers into a vector
     replace( numberStr.begin(), numberStr.end(), '-', ' ');
@@ -136,18 +133,14 @@ int main() {
     while (numberStrStream >> number)
         numbers.push_back(number);
 
-    // get target number
-    cout << "Please enter a target number: \n";
-    int goal;
-    cin >> goal;
+    int goal = atoi(argv[2]);
+
+    int nsols = 1;
+    if (argc>3)
+        nsols= atoi(argv[3]);
 
     // solve the problem
-    vector<string> solution = solve(numbers, goal);
-
-    // print the solution
-    for (auto const& step: solution) {
-        cout << step << "\n";
-    }
+    solve(numbers, goal, nsols);
 
     return 0;
 }
